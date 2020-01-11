fa8973e9a3dcf36605f761aa6b624ef6df95228f
Peer Naupold <greenion@gmx.de>
2020-01-11 02:05:24 +0100

    added functionality of  adding a task

--- app.py
+++ app.py
@@ -9,11 +9,19 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-
-@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=["POST"])
+def insert_task():
+    tasks= mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
